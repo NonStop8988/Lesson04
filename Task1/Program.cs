d7710b854b2e521feb74d7da36e81c8184fc7407
Maxim <lyskomax2@mail.ru>
2024-01-18 19:23:54 +0300

    Add Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,27 +1,5 @@
 ﻿// Задача №1. Создать двумерный массив с размерами 3 x 5, состоящий из целых чисел. Вывести его элементы на экран
 
-// int[,] matrix = new int[3, 5];
-
-// // Создаем массив, проходимся по каждой строке двумерного массива
-// Random rnd = new Random();
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         matrix[i, j] = rnd.Next(1, 11);
-//     }
-// }
-
-// // Выводим массив на экран
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         Console.Write($"{matrix[i, j]} ");
-//     }
-//     Console.WriteLine(); // Переход на новую строку
-// }
-
 // Функция - создание двумерного целочисленного массива
 int[,] CreateMatrix(int rowCount, int columsCount)
 {
---
+++ Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача №2. Назовём число «интересным» если его сумма цифр чётная. Создать двумерный массив, состоящий из целых чисел. Вывести на экран «интересные» элементы массива.
+
+// Функция - создание двумерного целочисленного массива
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+// Функция - подсчет суммы цифр числа
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    return sum;
+}
+
+// Функция - Проверка элемента на интересность (четность)
+bool IsInteresting(int value)
+{
+    int sumOfDigits = GetSumOfDigits(value);
+    if (sumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+// Функция - вывод двумерного массива на экран
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"Строка {i} - "); // Название каждой строки
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine(); // Переход на новую строку
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+}
