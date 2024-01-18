b17e3863b5b740cd0cfc07f20186361dec9a13a8
Maxim <lyskomax2@mail.ru>
2024-01-18 19:59:27 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №3. Считать с консоли строку, состоящую из цифр и латинских букв. Сформировать новую строку, состоящую из букв исходной строки.
+
+string GetLettersFromString(string s)
+{
+    string leters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            leters += e;
+        }
+    }
+    return leters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
